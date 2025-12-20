15371e0ec46734e25f91cc32201e5a6e171d5689
Bruno-R21 <braugusto91@gmail.com>
2025-12-20 13:05:57 -0300

    Desafio 2
    Código para escrever mensagem com o seu nome

---
+++ Python_Projetos/01. Curso_Guanabara/Desafio 002.py
@@ -0,0 +1,11 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+meu_nome = input('Digite o seu nome:')
+msg = meu_nome
+
+print('Olá', meu_nome, ', que bom te ver por aqui!')
+
+print('E aí{}, que tal estudarmos novos códigos?'.format(msg))
+
+      
+
