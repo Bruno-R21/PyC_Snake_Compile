6c3af0bcf82c795ad93634f119cfc97f916139d0
Bruno-R21 <braugusto91@gmail.com>
2026-01-16 10:22:22 -0300

    Des021

--- PyProjetos/Curso_Guanabara/Desafio 021.py
+++ PyProjetos/Curso_Guanabara/Desafio 021.py
@@ -3,11 +3,11 @@
 '''import pygame 
 pygame.init()
 
-pygame.mixer.music.load('Sorriso.mp3')
+pygame.mixer.music.load('Sorriso.wav')
 pygame.mixer.music.play()
 pygame.event.wait()'''
 
-'''import pygame
+import pygame
 import os
 pygame.init()
 
@@ -24,29 +24,7 @@ if os.path.exists('Sorriso.wav'):
         clock.tick(10)
 
 else:
-    print('O arquivo Sorriso.wav não funcionando')'''
+    print('O arquivo Sorriso.wav não funcionando')
 
 #Infelizmente por problemas de atualização do Python e da descontinuidade da bliblioteca pygame, não consegui fazer o código funcionar.     
-
-import pygame
-import os
-pygame.init()
-
-if os.path.exists('Sorriso.wav'):
-    pygame.mixer.music.load('Sorriso.wav')
-    pygame.mixer.music.play()
-    pygame.mixer.music.set_volume(1)
-
-    clock = pygame.time.Clock()  # Corrigido: pygame.time.Clock() (com maiúscula)
-    clock.tick(10)
-
-    while pygame.mixer.music.get_busy():
-        pygame.event.wait()  # Corrigido: usar wait() para aguardar eventos, em vez de poll()
-        clock.tick(10)
-
-else:
-    print('O arquivo Sorriso.wav não foi encontrado.')
-
-
-
 
