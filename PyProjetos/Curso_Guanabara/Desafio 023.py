b4fa0fb9a67ba9cedef2902586dadb9b09412079
Bruno-R21 <braugusto91@gmail.com>
2026-01-22 21:19:01 -0300

    Desafio 023
    Unidade, dezena, centena e milhar.

---
+++ PyProjetos/Curso_Guanabara/Desafio 023.py
@@ -0,0 +1,18 @@
+'''(Desafio 23) Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+Ex.: Digite um número: 2501
+unidade: 1 - dezena: 0 - centena: 5 - milhar: 2'''
+
+
+
+'''num = str(input('Digite um número qualquer, pode ser de 0 até 9999:'))
+num.split()
+
+unidade = num[4]
+dezena = num[3]
+centena = num[2]
+milhar = num[1]
+
+print(f'Unidade: {unidade} \nDezena: {dezena} \nCentena: {centena} \nMilhar: {milhar}')'''
+
+
+
