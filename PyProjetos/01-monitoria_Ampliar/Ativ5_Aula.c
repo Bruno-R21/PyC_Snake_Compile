273dfbbedd40bb0b2c69a55231bcf7b33f8ed2df
Bruno-R21 <bruno21.conta.estudante@gmail.com>
2026-04-12 20:07:14 -0300

    5º commit em C - Criando as Atividades da Monitoria Ampliar.

---
+++ PyProjetos/01-monitoria_Ampliar/Ativ5_Aula.c
@@ -0,0 +1,5 @@
+/* 
+Abaixo segue o código criado na Atv4.
+Refatore o código para atender a seguinte regra de negócio: Quando o cliente informar um código inválido, o programa deverá retornar ao menu solicitando que insira novamente a opção.
+*/
+
