c1b6377a0fd04e3cdf63f29cc876c6ccee65ecca
Bruno-R21 <bruno21.conta.estudante@gmail.com>
2026-04-12 21:43:26 -0300

    9º commit em C - Criando o arquivo Tarefa14_Casa.

---
+++ PyProjetos/01-monitoria_Ampliar/Tarefa14_Casa.c
@@ -0,0 +1,19 @@
+/*
+    Escreva um algoritmo em C que leia o nome de uma pessoa.
+    O programa deverá escrever na tela "Bem Vindo [nome da pessoa]".
+*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    char nome[50];
+
+    printf("Informe o seu primeiro nome: ");
+    scanf("%49s", nome);
+
+    printf("\nOlá %s, seja bem-vindo!", nome);
+
+    return 0;
+} 
